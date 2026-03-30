d39f554204bc76b35038c4cf7be8039180ac75ad
bryan_silva <bryan.s.silva@edu.senai.br>
2026-03-30 08:31:45 -0300

    12 media_e_conceito.py

--- 12 media_e_conceito.py
+++ 12 media_e_conceito.py
@@ -1,15 +1,12 @@
 # Programa que calcula média e conceito do aluno
 
-# Entrada das notas
 n1 = float(input("Nota 1: "))
 n2 = float(input("Nota 2: "))
 n3 = float(input("Nota 3: "))
 n4 = float(input("Nota 4: "))
 
-# Cálculo da média
 media = (n1 + n2 + n3 + n4) / 4
 
-# Define conceito
 if media >= 9:
     conceito = "A"
 elif media >= 7.5:
@@ -21,13 +18,11 @@ elif media >= 4:
 else:
     conceito = "E"
 
-# Define situação
 if conceito in ["A", "B", "C"]:
     status = "APROVADO"
 else:
     status = "REPROVADO"
 
-# Saída
 print(f"Notas: {n1}, {n2}, {n3}, {n4}")
 print(f"Média: {media:.2f}")
 print(f"Conceito: {conceito}")

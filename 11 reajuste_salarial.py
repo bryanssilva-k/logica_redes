f2f3983f26e8323f87c632a4a4f08c15ae74bb9f
bryan_silva <bryan.s.silva@edu.senai.br>
2026-03-30 08:21:28 -0300

    Update 11 reajuste_salarial.py

--- 11 reajuste_salarial.py
+++ 11 reajuste_salarial.py
@@ -1,5 +1,7 @@
+# Programa que calcula reajuste salarial
 salario = float(input("Digite o salário: "))
 
+# Define percentual de aumento
 if salario <= 1000:
     percentual = 0.20
 elif salario <= 1700:
@@ -9,9 +11,11 @@ elif salario <= 2300:
 else:
     percentual = 0.05
 
+# Calcula valores
 aumento = salario * percentual
 novo = salario + aumento
 
+# Exibe resultados
 print(f"Salário antigo: R$ {salario:.2f}")
 print(f"Aumento: {percentual*100}%")
 print(f"Valor do aumento: R$ {aumento:.2f}")
